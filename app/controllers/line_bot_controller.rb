071c37ae9d1a8ecd4b6a67bbb39519bae25e277a
hodumi <forsite2501@gamil.com>
2023-07-22 00:28:01 +0900

    no message

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -3,6 +3,33 @@ class LineBotController < ApplicationController
     protect_from_forgery except: [:callback]
 
     def callback
+
+      body = request.body.read
+
+      # 悪意のあるサーバーからのリクエストを検証する↓
+      signature = request.env['HTTP_X_LINE_SIGNATURE']
+        unless client.validate_signature(body, signature)
+        return head :bad_request
+      end
+
+      # LINEの送信メッセージのボディを配列に変換↓
+      events = client.parse_events_from(body)
+      #送信されたメッセージがテキストメッセージかどうかの検証↓
+      events.each do |event|
+        case event
+        when Line::Bot::Event::Message
+          case event.type
+            when Line::Bot::Event::MessageType::Text
+              #返信メッセージ↓を作成
+              message = {
+                type: 'text',
+                text: event.message['text']
+              }
+              client.reply_message(event['replyToken'], message)
+          end
+        end
+      end
+      head :ok
     end
 
 
---
+++ app/ignere/test.rb
@@ -0,0 +1,27 @@
+Parameters: {
+    "destination"=>"U6ce9ddeceb39692892a00e4f91bddbd5",
+    "events"=>
+    [{
+        "type"=>"message",
+         "message"=>{
+            "type"=>"text",
+             "id"=>"464925101784826338",
+              "text"=>"ななな"},
+    "webhookEventId"=>"01H5WC81FVGJVWK1R389CG8ANE",
+     "deliveryContext"=>{"isRedelivery"=>false},
+    "timestamp"=>1689948259408,
+     "source"=>{"type"=>"user", "userId"=>"Udae774449265cca964d1951a7ba2605f"},
+    "replyToken"=>"[FILTERED]", "mode"=>"active"}],
+    "line_bot"=>{"destination"=>"U6ce9ddeceb39692892a00e4f91bddbd5",
+    "events"=>[{"type"=>"message",
+    "message"=>{"type"=>"text", "id"=>"464925101784826338", "text"=>"ななな"},
+    "webhookEventId"=>"01H5WC81FVGJVWK1R389CG8ANE",
+    "deliveryContext"=>{"isRedelivery"=>false},
+    "timestamp"=>1689948259408,
+    "source"=>{"type"=>"user",
+    "userId"=>"Udae774449265cca964d1951a7ba2605f"},
+    "replyToken"=>"[FILTERED]", "mode"=>"active"}]}}
+
+
+"{\"destination\":\"U6ce9ddeceb39692892a00e4f91bddbd5\",\"events\":[{\"type\":\"message\",\"message\":{\"type\":\"text\",\"id\":\"464925101784826338\",\"text\":\"\xE3\x81\xAA\xE3\x81\xAA\xE3\x81\xAA\"},\"webhookEventId\":\"01H5WC81FVGJVWK1R389CG8ANE\",\"deliveryContext\":{\"isRedelivery\":false},\"timestamp\":1689948259408,\"source\":{\"type\":\"user\",\"userId\":\"Udae774449265cca964d1951a7ba2605f\"},\"replyToken\":\"1ca3b3f5214b40219db9a48e5dd1ce63\",\"mode\":\"active\"}]
+}"
