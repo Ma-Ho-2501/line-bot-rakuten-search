2b19bea37a3a429813b0ae5caeefdb1554572966
hodumi <forsite2501@gamil.com>
2023-07-23 15:29:34 +0900

    2023/07/23 アプリ完成

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -54,150 +54,166 @@ class LineBotController < ApplicationController
           c.affiliate_id = ENV['RAKUTEN_AFID']
         end
 
-        res = RakutenWebService::Ichiba::Item.search(keyword: input, hits: 5,formatVersion: 2,availability: 1,orFlag: 0)
+        res = RakutenWebService::Ichiba::Item.search(keyword: input, hits: 5,formatVersion: 2,availability: 1,orFlag: 0,imageFlag: 1)
         items = []
         items = res.map{|item| item}
-        # make_reply_content(items)
-
-        #############################################################################
-          text = ''
-            items.each do |item|
-            text <<
-              item['mediumImageUrls'].first + "\n" +
-              item['itemName'] + "\n" +
-              item['itemPrice'].to_s+ '円'  + "\n" +
-              item['itemUrl'] + "\n" +
-              # item['itemCaption'] + "\n" + #####7/23キャプションこの部分がエラー出る #####
-              item['shopName'] + "\n" +
-              "\n"
-          end
-
-        message = {
-          type: 'text',
-          text: text
-        }
+        make_reply_content(items)
+        end
 
         #############################################################################
 
-    end
+        # text = ''
+        #   items.each do |item|
+        #   text <<
+        #   item['shopName'] + "\n" +
+        #   item['itemName'] + "\n" +
+        #   item['itemPrice'].to_s+ '円'  + "\n" +
+        #   item['catchcopy'] + "\n" +
+        #   item['mediumImageUrls'].first + "\n" +
+        #   item['itemUrl'] + "\n" +
+        #     "\n"
+        # end
+        #############################################################################
 
-    # def make_reply_content(items)
-    #   {
-    #     type: 'flex',
-    #     altText: '楽天商品検索の結果です。',
-    #     contents:set_carousel(response['items'])
-    #   }
-    # end
 
-    # def set_carousel(items)
-    #   bubbles = []
-    #   items.each do |item|
-    #     bubbles.push set_bubble(item)
-    #   end
-    #   {
-    #     type: 'carousel',
-    #     contents: bubbles
-    #   }
-    # end
+        def make_reply_content(items)
+          {
+            "type": 'flex',
+            "altText": 'This is a Flex Message',
+            "contents":
+            {
+              "type": 'carousel',
+              "contents": [
+                make_part(items[0]),
+                make_part(items[1]),
+                make_part(items[2]),
+                make_part(items[3]),
+                make_part(items[4])
+              ]
+            }
+          }
+        end
 
-    # def set_bubble(item)
-    #   {
-    #     type: 'bubble',
-    #     hero: set_hero(item),
-    #     body: set_body(item),
-    #     footer: set_footer(item)
-    #   }
-    # end
+        def make_part(item)
+          image = item['mediumImageUrls'].first
+          itemtitle = item['itemName']
+          price = item['itemPrice'].to_s
+          shoptitle = item['shopName']
+          catchcopy = item['catchcopy']
+          url = item['itemUrl']
+          { 
+            "type": "bubble",
+            "hero": {
+              "type": "image",
+              "size": "full",
+              "aspectRatio": "20:17",
+              "aspectMode": "fit",
+              "url": image
+            },
+            
+            "body": {
+              "type": "box",
+              "layout": "vertical",
+              "spacing": "sm",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": itemtitle,
+                  "wrap": false,
+                  "weight": "bold",
+                  "size": "lg"
+                },
+                {
+                  "type": "box",
+                  "layout": "baseline",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "価格：",
+                      "size": "xxs",
+                      "flex": 0
+                    },
+                    {
+                      "type": "text",
+                      "text": price,
+                      "wrap": true,
+                      "weight": "bold",
+                      "size": "3xl",
+                      "flex": 0,
+                      "align": "start"
+                    },
+                    {
+                      "type": "text",
+                      "text": "円",
+                      "wrap": true,
+                      "weight": "bold",
+                      "size": "sm",
+                      "flex": 0,
+                      "align": "start"
+                    }
+                  ],
+                  "margin": "md"
+                },
+                {
+                  "type": "box",
+                  "layout": "baseline",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "店舗：",
+                      "size": "xxs",
+                      "flex": 0
+                    },
+                    {
+                      "type": "text",
+                      "text": shoptitle,
+                      "weight": "bold",
+                      "flex": 1
+                    }
+                  ],
+                  "margin": "none"
+                },
+                {
+                  "type": "box",
+                  "layout": "vertical",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": catchcopy,
+                      "wrap": true,
+                      "size": "xxs",
+                      "style": "normal",
+                      "maxLines": 5
+                    }
+                  ],
+                  "backgroundColor": "#EEEEEE",
+                  "cornerRadius": "3px",
+                  "paddingTop": "lg",
+                  "paddingBottom": "lg",
+                  "paddingStart": "md",
+                  "paddingEnd": "md"
+                }
+              ]
+            },
+            "footer": {
+              "type": "box",
+              "layout": "vertical",
+              "spacing": "sm",
+              "contents": [
+                {
+                  "type": "button",
+                  "style": "primary",
+                  "action": {
+                    "type": "uri",
+                    "label": "商品ページへGO！",
+                    "uri": url
+                  }
+                }
+              ]
+            }
+           }
+        end
 
-    # def set_hero(item)
-    #   {
-    #     type: "image",
-    #     size: "full",
-    #     aspectRatio: "20:17",
-    #     aspectMode: "cover",
-    #     url: item['mediumImageUrls'].first
-    #   }
-    # end
-
-    # def set_body(hotel)
-    #   {
-    #     "type": "box",
-    #     "layout": "vertical",
-    #     "spacing": "sm",
-    #     "contents": [
-    #       {
-    #         "type": "text",
-    #         "text": item['itemName'],
-    #         "wrap": false,
-    #         "weight": "bold",
-    #         "size": "lg",
-    #         "color": "#0068E9"
-    #       },
-    #       {
-    #         "type": "box",
-    #         "layout": "baseline",
-    #         "contents": [
-    #           {
-    #             "type": "text",
-    #             "text": item['itemPrice'].to_s,
-    #             "wrap": true,
-    #             "weight": "bold",
-    #             "size": "xxl",
-    #             "flex": 0
-    #           },
-    #           {
-    #             "type": "text",
-    #             "text": "円",
-    #             "wrap": true,
-    #             "weight": "bold",
-    #             "size": "sm",
-    #             "flex": 0
-    #           }
-    #         ]
-    #       },
-    #       {
-    #         "type": "box",
-    #         "layout": "vertical",
-    #         "contents": [
-    #           {
-    #             "type": "text",
-    #             "text": item['shopName'],
-    #             "margin": "none"
-    #           },
-    #           # {
-    #           #   "type": "text",
-    #           #   "text": item['itemCaption'],
-    #           #   "margin": "sm",
-    #           #   "size": "sm",
-    #           #   "wrap": true,
-    #           #   "maxLines": 4
-    #           # }
-    #         ],
-    #         "margin": "md"
-    #       }
-    #     ]
-    #   }
-    # end
-
-    # def set_footer(hotel)
-    #   {
-    #     "type": "box",
-    #     "layout": "vertical",
-    #     "spacing": "sm",
-    #     "contents": [
-    #       {
-    #         "type": "button",
-    #         "style": "primary",
-    #         "action": {
-    #           "type": "uri",
-    #           "label": "商品ページへGO！",
-    #           "uri":  item['itemUrl']
-    #         }
-    #       }
-    #     ],
-    #     "margin": "xl"
-    #   }
-    # end
 
 
 end
--- app/ignere/flex.rb
+++ app/ignere/flex.rb
@@ -1,196 +1,217 @@
 {
-    "type": "carousel",
-    "contents": [
-      {
-        "type": "bubble",
-        "hero": {
-          "type": "image",
-          "size": "full",
-          "aspectRatio": "20:17",
-          "aspectMode": "cover",
-          "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png"
-        },
-        "body": {
-          "type": "box",
-          "layout": "vertical",
-          "spacing": "sm",
-          "contents": [
-            {
-              "type": "text",
-              "text": "キングダム/漫画全巻セット◆新品Ss≪1～69巻（既刊）≫【即納】【コンビニ受取/郵便局受取対応",
-              "wrap": false,
-              "weight": "bold",
-              "size": "lg",
-              "color": "#0068E9"
-            },
-            {
-              "type": "box",
-              "layout": "baseline",
-              "contents": [
-                {
-                  "type": "text",
-                  "text": "3,000",
-                  "wrap": true,
-                  "weight": "bold",
-                  "size": "xxl",
-                  "flex": 0
-                },
-                {
-                  "type": "text",
-                  "text": "円",
-                  "wrap": true,
-                  "weight": "bold",
-                  "size": "sm",
-                  "flex": 0
-                }
-              ]
-            },
-            {
-              "type": "box",
-              "layout": "vertical",
-              "contents": [
-                {
-                  "type": "text",
-                  "text": "【アイリスオーヤマ】",
-                  "margin": "none"
-                },
-                {
-                  "type": "text",
-                  "text": "◆たっぷりしまえる丈夫な造り全体耐荷重は約35kg、天板・棚板は約7kg板の厚さは1.6cmでしっかりとした造りです。◆コンパクトサイズだから置き場は自由自在天板・棚板は約7kg板の厚さは1.6cmでしっかりとした造りです。◆コンパクトサイズだから置き場は自由自在天板・棚板は約7kg板の厚さは1.6cmでしっかりとした造りです。◆コンパクトサイズだから置き場は自由自在天板・棚板は約7kg板の厚さは1.6cmでしっかりとした造りです。◆コンパクトサイズだから置き場は自由自在",
-                  "action": {
-                    "type": "uri",
-                    "label": "action",
-                    "uri": "http://linecorp.com/"
-                  },
-                  "margin": "sm",
-                  "size": "sm",
-                  "wrap": true,
-                  "maxLines": 4
-                }
-              ],
-              "margin": "md"
-            }
-          ]
-        },
-        "footer": {
-          "type": "box",
-          "layout": "vertical",
-          "spacing": "sm",
-          "contents": [
-            {
-              "type": "button",
-              "style": "primary",
-              "action": {
-                "type": "uri",
-                "label": "商品ページへGO！",
-                "uri": "https://linecorp.com"
-              }
-            }
-          ],
-          "margin": "xl"
-        }
+  "type": "carousel",
+  "contents": [
+    {
+      "type": "bubble",
+      "hero": {
+        "type": "image",
+        "size": "full",
+        "aspectRatio": "20:17",
+        "aspectMode": "cover",
+        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png?商品画像"
       },
-      {
-        "type": "bubble",
-        "hero": {
-          "type": "image",
-          "size": "full",
-          "aspectRatio": "20:13",
-          "aspectMode": "cover",
-          "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_6_carousel.png"
-        },
-        "body": {
-          "type": "box",
-          "layout": "vertical",
-          "spacing": "sm",
-          "contents": [
-            {
-              "type": "text",
-              "text": "Metal Desk Lamp",
-              "wrap": true,
-              "weight": "bold",
-              "size": "xl"
-            },
-            {
-              "type": "box",
-              "layout": "baseline",
-              "flex": 1,
-              "contents": [
-                {
-                  "type": "text",
-                  "text": "$11",
-                  "wrap": true,
-                  "weight": "bold",
-                  "size": "xl",
-                  "flex": 0
-                },
-                {
-                  "type": "text",
-                  "text": ".99",
-                  "wrap": true,
-                  "weight": "bold",
-                  "size": "sm",
-                  "flex": 0
-                }
-              ]
-            },
-            {
-              "type": "text",
-              "text": "Temporarily out of stock",
-              "wrap": true,
-              "size": "xxs",
-              "margin": "md",
-              "color": "#ff5551",
-              "flex": 0
-            }
-          ]
-        },
-        "footer": {
-          "type": "box",
-          "layout": "vertical",
-          "spacing": "sm",
-          "contents": [
-            {
-              "type": "button",
-              "flex": 2,
-              "style": "primary",
-              "color": "#aaaaaa",
-              "action": {
-                "type": "uri",
-                "label": "Add to Cart",
-                "uri": "https://linecorp.com"
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "text",
+            "text": "インテリアに馴染むシンプルでおしゃれな壁掛けカレンダー",
+            "wrap": false,
+            "weight": "bold",
+            "size": "lg"
+          },
+          {
+            "type": "box",
+            "layout": "baseline",
+            "contents": [
+              {
+                "type": "text",
+                "text": "価格：",
+                "size": "xxs",
+                "flex": 0
+              },
+              {
+                "type": "text",
+                "text": "4,450",
+                "wrap": true,
+                "weight": "bold",
+                "size": "3xl",
+                "flex": 0,
+                "align": "start"
+              },
+              {
+                "type": "text",
+                "text": "円",
+                "wrap": true,
+                "weight": "bold",
+                "size": "sm",
+                "flex": 0,
+                "align": "start"
               }
-            },
-            {
-              "type": "button",
-              "action": {
-                "type": "uri",
-                "label": "Add to wish list",
-                "uri": "https://linecorp.com"
+            ],
+            "margin": "md"
+          },
+          {
+            "type": "box",
+            "layout": "baseline",
+            "contents": [
+              {
+                "type": "text",
+                "text": "店舗：",
+                "size": "xxs",
+                "flex": 0
+              },
+              {
+                "type": "text",
+                "text": "楽天ブックス",
+                "weight": "bold",
+                "flex": 1
               }
-            }
-          ]
-        }
+            ],
+            "margin": "none"
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "text",
+                "text": "「2023年版 Wall Calendar」インテリアに馴染むシンプルでおしゃれな壁掛けカレンダー（※2023年1月〜12月分） 【名入れ イラスト刻印 ラッピング 対応】r」インテリアに馴染むシンプルでおしゃれな壁掛けカレンダー（※2023年1月〜12月分） 【名入れ イラスト刻印 ラッピ",
+                "wrap": true,
+                "size": "xxs",
+                "style": "normal",
+                "maxLines": 5
+              }
+            ],
+            "backgroundColor": "#EEEEEE",
+            "cornerRadius": "3px",
+            "paddingTop": "lg",
+            "paddingBottom": "lg",
+            "paddingStart": "md",
+            "paddingEnd": "md"
+          }
+        ]
       },
-      {
-        "type": "bubble",
-        "body": {
-          "type": "box",
-          "layout": "vertical",
-          "spacing": "sm",
-          "contents": [
-            {
-              "type": "button",
-              "flex": 1,
-              "gravity": "center",
-              "action": {
-                "type": "uri",
-                "label": "See more",
-                "uri": "https://linecorp.com"
-              }
+      "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "button",
+            "style": "primary",
+            "action": {
+              "type": "uri",
+              "label": "商品ページへGO！",
+              "uri": "https://item.rakuten.co.jp/etranger/clg43-a2-01/?商品リンク"
             }
-          ]
-        }
+          }
+        ]
       }
-    ]
-  }
+    },
+    {
+      "type": "bubble",
+      "hero": {
+        "type": "image",
+        "size": "full",
+        "aspectRatio": "20:13",
+        "aspectMode": "cover",
+        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_6_carousel.png"
+      },
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "text",
+            "text": "Metal Desk Lamp",
+            "wrap": true,
+            "weight": "bold",
+            "size": "xl"
+          },
+          {
+            "type": "box",
+            "layout": "baseline",
+            "flex": 1,
+            "contents": [
+              {
+                "type": "text",
+                "text": "$11",
+                "wrap": true,
+                "weight": "bold",
+                "size": "xl",
+                "flex": 0
+              },
+              {
+                "type": "text",
+                "text": ".99",
+                "wrap": true,
+                "weight": "bold",
+                "size": "sm",
+                "flex": 0
+              }
+            ]
+          },
+          {
+            "type": "text",
+            "text": "Temporarily out of stock",
+            "wrap": true,
+            "size": "xxs",
+            "margin": "md",
+            "color": "#ff5551",
+            "flex": 0
+          }
+        ]
+      },
+      "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "button",
+            "flex": 2,
+            "style": "primary",
+            "color": "#aaaaaa",
+            "action": {
+              "type": "uri",
+              "label": "Add to Cart",
+              "uri": "https://linecorp.com"
+            }
+          },
+          {
+            "type": "button",
+            "action": {
+              "type": "uri",
+              "label": "Add to wish list",
+              "uri": "https://linecorp.com"
+            }
+          }
+        ]
+      }
+    },
+    {
+      "type": "bubble",
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "button",
+            "flex": 1,
+            "gravity": "center",
+            "action": {
+              "type": "uri",
+              "label": "See more",
+              "uri": "https://linecorp.com"
+            }
+          }
+        ]
+      }
+    }
+  ]
+}
